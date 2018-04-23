dd9cf2a01dc9325e6c2dbba51eb223e62cf96f52
wxisme <850885154@qq.com>
2018-04-23 19:28:31 +0800

    message digest

--- common/message_digest.py
+++ common/message_digest.py
@@ -3,3 +3,16 @@
 import hashlib
 
 
+class MessageDigestUtils:
+
+        __md5 = hashlib.md5()
+        __charset = "utf-8"
+
+        @classmethod
+        def create_hashes(cls, data, hashes):
+            result = [0] * hashes
+            k = 0
+            salt = 0
+            while k < hashes:
+                
+
