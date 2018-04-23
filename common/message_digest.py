acd1e49f3994292bc9ec9b6e383b96f702134024
wxisme <850885154@qq.com>
2018-04-24 00:08:41 +0800

    md5

--- common/message_digest.py
+++ common/message_digest.py
@@ -3,7 +3,7 @@
 import hashlib
 
 
-class MessageDigestUtils:
+class MessageDigestUtils(object):
 
         __md5 = hashlib.md5()
         __charset = "utf-8"
@@ -14,5 +14,9 @@ class MessageDigestUtils:
             k = 0
             salt = 0
             while k < hashes:
-                
+                cls.__md5.update(salt)
+                cls.__md5.update(data)
+                salt += 1
+                digest = cls.__md5.digest()
+
 
