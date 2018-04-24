560582273c3683c7676fb5899060da12c9379b8c
wxisme <850885154@qq.com>
2018-04-24 22:49:08 +0800

    fix test code

--- test/bloomfilter_test.py
+++ test/bloomfilter_test.py
@@ -1,6 +1,8 @@
-from common.bloomfilter import BloomFilter
+import redis
+
 from bitset.bitset import BitSet
 from bitset.redis_bitset import RedisBitSet
+from common.bloomfilter import BloomFilter
 
 # bitset
 bloomfilter = BloomFilter(0.001, 10000, BitSet())
@@ -9,8 +11,8 @@ print(bloomfilter.contains('123'))
 print(bloomfilter.contains('456'))
 
 # redis bitset
-bloomfilter2 = BloomFilter(0.001, 10000, RedisBitSet())
+bloomfilter2 = BloomFilter(0.001, 10000, RedisBitSet('bloomfilter:key:name',
+                                                     redis.Redis(host='127.0.0.1', port=6379, password='123456')))
 bloomfilter2.add('123')
 print(bloomfilter2.contains('123'))
 print(bloomfilter2.contains('456'))
-
